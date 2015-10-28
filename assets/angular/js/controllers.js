2c7128bf9ed4c2c96daf9f7f2aba75e4cee9ec4e
iancottam91 <icottam@msn.com>
2015-10-28 10:13:57 +0000

    Add skill and logos to project details page

--- assets/angular/js/controllers.js
+++ assets/angular/js/controllers.js
@@ -32,12 +32,29 @@ porfolioControllers.controller('ProjectDetailCtrl', ['$scope', '$routeParams', '
 
   function($scope, $routeParams, $http, $ocLazyLoad, $sce) {
 
-
+    // $scope.skillOnShow = "";
 
     // load default option
     $http.get('assets/angular/json/projects/fca-handbook.json').success(function(data) {
       $scope.project = data;
+      $scope.skillOnShow = 'jquery';
     });
+
+    $scope.changeSkill = function(val){
+      $scope.skillOnShow = val;
+      console.log(val);
+    }
+
+    $scope.desc = "true";
+    // the structure of the JSON is tricky to get right, we might need to access a file, by it's skill name. This is the thinking behind fca-handbook.json
+
+
+
+    // handle click to change logo information
+
+    // Have a central skill id to img JSON/info
+
+    // Have a per project skill usage description
   
 
 
