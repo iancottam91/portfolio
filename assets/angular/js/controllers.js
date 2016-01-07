852f5f89e2081e3662281b831f4a5584a2755e3b
iancottam91 <icottam@msn.com>
2016-01-07 12:27:41 +0000

    Add filters for the projet page

--- assets/angular/js/controllers.js
+++ assets/angular/js/controllers.js
@@ -25,6 +25,109 @@ porfolioControllers.controller('ProjectsCtrl', ['$scope', '$routeParams', '$http
       $scope.projects = data;
     });
 
+    // list checkbox filters
+
+    $scope.skillsCheckboxes = [
+      'jQuery',
+      'JavaScript',
+      'JSON',
+      'php',
+      'Drupal',
+      'Sass ',
+      'Compass',
+      'Less',
+      'AngularJS',
+      'EmberJS',
+      'Wordpress',
+      'apache',
+      'NWjs',
+      'CSS_Regression_Testing',
+      'AJAX',
+      'Local_Storage',
+      'Freemarker',
+      'stylus'
+    ];
+
+    $scope.selectedSkills = [
+      'jQuery',
+      'JavaScript',
+      'JSON',
+      'php',
+      'Drupal',
+      'Sass ',
+      'Compass',
+      'Less',
+      'AngularJS',
+      'EmberJS',
+      'Wordpress',
+      'apache',
+      'NWjs',
+      'CSS_Regression_Testing',
+      'AJAX',
+      'Local_Storage',
+      'Freemarker',
+      'stylus'
+    ],
+
+    // better to have this the other way round i think - like asos
+
+
+    // if NONE of the skills matching a project and selected, then remove the project from the array
+    $scope.skillFilter = function(projects){
+
+      if ($scope.selectedSkills.length > 0) {
+
+        console.log(projects.skills.length);
+
+        for(i=0;i<projects.skills.length;i++){   
+
+
+          // return to filter out
+          // (more complex as shop is skill is not unique for each project!)
+
+          // if the skill is not in selectedSkills, check the next one, on the last go return if there are no matches.
+
+          // if the skill is in selected skills:
+          if ($.inArray(projects.skills[i], $scope.selectedSkills) >= 0) {
+            // one match is enough, that's good, break from the loop and dont remove the project
+
+             console.log('match for:');
+             console.log(projects.skills[i]);
+             console.log(projects.title);
+
+            break;
+          } else {
+            // if you get to the final skill and that doesnt match, then return and remove the project from the array
+            if(i == projects.skills.length - 1){
+              return
+            }
+          }
+
+        }
+
+      }
+
+      return projects
+    }
+
+    // toggle selection for a given fruit by name
+    $scope.toggleSelection = function toggleSelection(fruitName) {
+      var idx = $scope.selectedSkills.indexOf(fruitName);
+
+      // is currently selected
+      if (idx > -1) {
+        $scope.selectedSkills.splice(idx, 1);
+      }
+
+      // is newly selected
+      else {
+        $scope.selectedSkills.push(fruitName);
+      }
+
+      console.log($scope.selectedSkills);
+
+    };
+
 
   }]);
 
@@ -33,11 +136,13 @@ porfolioControllers.controller('ProjectDetailCtrl', ['$scope', '$routeParams', '
   function($scope, $routeParams, $http,  $sce,  $location, $anchorScroll) {
 
 
+    // default slideshow: as no slider
     $scope.slides = false;
 
+    // project title
     $scope.projectTitle = $routeParams.projectTitle;
 
-    // load default option
+    // load model and set default options
     $http.get('assets/angular/json/projects/' + $routeParams.projectTitle + '.json').success(function(data) {
       $scope.project = data;
       $scope.skillOnShow = 'javascript';
@@ -47,6 +152,7 @@ porfolioControllers.controller('ProjectDetailCtrl', ['$scope', '$routeParams', '
       }
 
     });
+
 
     $scope.descUrl = "/assets/angular/html/projects/" + $scope.projectTitle + ".html";
 
